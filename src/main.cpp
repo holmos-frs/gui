b33ca175f3f5ade3efdbd40f5906b4ac8cf0cb51
Niklas <graf.niks@gmail.com>
2018-01-11 18:48:08 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/* main.cpp
+ *
+ * Copyright (C) 2018 Niklas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtkmm.h>
+
+int main(int   argc,
+         char *argv[])
+{
+    auto app =
+      Gtk::Application::create(argc, argv,
+                               "org.gnome.Gui");
+
+    Gtk::Window window;
+    window.set_title ("gui");
+    window.set_default_size(200, 200);
+
+    /* You can add GTK+ widgets to your window here.
+     * See https://developer.gnome.org/ for help.
+     */
+
+    return app->run(window);
+}
