faec19dbeba6ddb6185e0a8b0fe70b246eb56c1d
Christoph Stelz <mail@ch-st.de>
2018-01-20 14:11:23 +0100

    matwidget in scrolledwindow to be resizable

--- src/ApplicationWindow.cpp
+++ src/ApplicationWindow.cpp
@@ -9,11 +9,13 @@ ApplicationWindow::ApplicationWindow() {
 	image = cv::imread("holmos_raw.png", 1);
 	std::cout << "image type " << image.type() << std::endl;
 	mat_widget = new MatWidget(&image);
+	m_scrollwindow = new Gtk::ScrolledWindow();
+	m_scrollwindow->add(*mat_widget);
 
 	m_label = new Gtk::Label("Hello World");
 
 	grid_container.add(*m_label);
-	grid_container.add(*mat_widget);
+	grid_container.add(*m_scrollwindow);
 
 	add(grid_container);
 
--- src/ApplicationWindow.h
+++ src/ApplicationWindow.h
@@ -13,6 +13,7 @@ public:
 
 private:
 	Gtk::Grid grid_container;
+	Gtk::ScrolledWindow *m_scrollwindow;
 	Gtk::Label *m_label;
 	MatWidget *mat_widget;
 	cv::Mat image;
